6b73b3e01e7662bf24331d7b1ee504fe775fa273
Sina Tootoonian <sina.tootoonian@gmail.com>
2023-09-05 14:17:31 +0100

    IntermittentGamma works.

--- corr_models.py
+++ corr_models.py
@@ -6,6 +6,9 @@ from sklearn.base import BaseEstimator
 import logging
 
 from scipy.stats import norm as norm_dist
+from scipy.stats import   gamma as gamma_dist
+from scipy.special import gamma as gamma_fun
+from scipy.special import digamma
 
 import fisher_plumes_tools as fpt
 import utils
@@ -38,19 +41,43 @@ def plot_data(y, labs, params = None, figsize=None):
     if params:
         plt.title(params2str(params))
 
-def plot_cdfs(y, mdls = [], figsize=None, n = 1001, gof_fun = fpt.compute_r2_value):
+def plot_cdfs(y, mdls = [], labs = [], figsize=None, n = 1001, gof_fun = fpt.compute_r2_value):
     cdf_true, xv = cdf_data(y)
     plt.plot(xv, cdf_true, "-",label="data" )
 
-    for mdl in mdls:
+    if len(labs) == 0:
+        labs = [str(mdl) for mdl in mdls]
+        
+    for mdl,lab in zip(mdls, labs):
         cdf_mdl = mdl.cdf(xv, mdl.params)
         gof = gof_fun(mdl.predict, y, n = n)
-        plt.plot(xv, cdf_mdl, "-", label=f"{str(mdl)}: {gof:.3f}")
+        plt.plot(xv, cdf_mdl, "-", label=f"{lab}: {gof:.3f}")
 
     plt.xlabel("x")
     plt.ylabel("P(data<x)")
     plt.legend()
-    
+
+def fixed_point_iterate(f, x0, max_iter = 1000, tol = 1e-6, damping = 0.5, verbose = False):
+    x = x0
+    for i in range(max_iter):
+        x_new = f(x)
+        if np.linalg.norm(x_new - x) < tol:
+            break
+        x = (1 - damping)*x_new + damping*x
+        if any(np.isnan(x)):
+            break
+
+    status = 0
+    if any(np.isnan(x)):
+        WARN("Nans encountered.")
+        status = 2
+    elif i == max_iter - 1:
+        WARN("Not converged.")
+        status = 1
+    else:
+        INFO(f"Converged to fixed point {x} in {i:>4d} iterations.")
+        
+    return x_new, status       
 
 class Exponential(BaseEstimator):
     Params = namedtuple('Params', ['λ', 'μ'])
@@ -125,9 +152,7 @@ class Exponential(BaseEstimator):
         assert y is None, "y must be None"
         y = X.flatten() # The data 
         return fpt.compare_cdfs(self.predict, y, cmp_fun = cmp_fun, n = n)
-
     
-
 class IntermittentExponential(Exponential):
     Params      = namedtuple('Params',      ['λ', 'μ', 'σ', 'γ'])
     HyperParams = namedtuple('HyperParams', ['σ_penalty', 'γ_pr_mean', 'γ_pr_strength'])
@@ -164,7 +189,7 @@ class IntermittentExponential(Exponential):
 
     def __init__(self, init_params, min_μ = 1e-6, σ_penalty=0, γ_pr_mean=0.5, γ_pr_strength=0):
         self.min_μ = min_μ
-        self.hyper_params= self.HyperParams(σ_penalty=0, γ_pr_mean=0.5, γ_pr_strength=0)
+        self.hyper_params= self.HyperParams(σ_penalty=σ_penalty, γ_pr_mean=γ_pr_mean, γ_pr_strength=γ_pr_strength)
         self.init_params = init_params
         self.params      = init_params
 
@@ -199,7 +224,7 @@ class IntermittentExponential(Exponential):
             if n0>0:
                 y0 = y[i0]
                 if self.hyper_params.σ_penalty > 0: σ2 = n0/(2*self.hyper_params.σ_penalty) * (-1 + np.sqrt(1 + 4 * self.hyper_params.σ_penalty * np.mean(y0**2)/n0))
-                else: σ2 = np.mean(y0**2)
+                else:                               σ2 = np.mean(y0**2)
             else:
                 σ2 = np.var(y)*1e-3 # Don't make it exactly 0
 
@@ -232,3 +257,133 @@ class IntermittentExponential(Exponential):
         labs[z & (y<0)]  = -1
         self.labs = labs
         
+class IntermittentGamma(IntermittentExponential):
+    Params      = namedtuple('Params',      ['λ', 'μ', 'σ', 'γ', 'k', 'm'])
+    HyperParams = namedtuple('HyperParams', ['σ_penalty', 'γ_pr_mean', 'γ_pr_strength'])
+    
+    @staticmethod
+    def agamma_cdf(y, params):
+        λ = params.λ
+        μ = params.μ
+        k = params.k
+        m = params.m
+    
+        Z0 = gamma_fun(m) * (μ**m)
+        Z1 = gamma_fun(k) * (λ**k)
+        Z  =  Z0 + Z1
+        cdf = 0*y
+        cdf[y<0]  = (1 -   gamma_dist.cdf(abs(y[y<0]), m, scale = μ)) * Z0/Z
+        cdf[y>=0] = Z0/Z + gamma_dist.cdf(y[y>=0],     k, scale = λ) * Z1/Z
+        return cdf
+
+    @staticmethod    
+    def cdf(y, params):
+        λ, μ, k, m, σ, γ = [params._asdict()[k] for k in ['λ', 'μ', 'k', 'm', 'σ', 'γ']]    
+        # Compute the CDF
+        R = IntermittentGamma.agamma_cdf(y, params)
+        H = norm_dist.cdf(y, scale = σ)
+        return γ*R + (1-γ)*H
+    
+    @staticmethod
+    def gen_data(M, params):
+        λ, μ, σ, γ, k, m = [params._asdict()[k] for k in ['λ', 'μ', 'σ', 'γ', 'k', 'm']]
+        y = np.zeros(M)
+        z = rand(M) < γ
+        i0= ~z
+        n0= np.sum(~z)
+        i1= z
+        n1= np.sum(z)
+        pp= λ**k * gamma_fun(k)/(λ**k * gamma_fun(k) + μ**m * gamma_fun(m))
+        ip= rand(M)<pp
+        # Set y at ip to be sampled from an exponential with mean λ
+        y[ip] = np.random.gamma(k, λ, sum(ip))
+        # Set y at ~ip to be sampled from an exponential with mean μ
+        y[~ip]= -np.random.gamma(m, μ, sum(~ip))       
+        y[~z] = randn(n0)*σ
+        
+        labs = 0*z
+        labs[z & (y>=0)] = 1
+        labs[z & (y<0)]  = -1
+
+        return y, labs
+
+    def fit(self, X, y=None, max_iter = 1001, tol = 1e-6, damping = 0.5, max_fp_iter=1000):
+        assert y is None, "y must be None"
+        y = X.flatten()
+
+        # Initialize
+        M     = len(y)
+
+        # Initialize the parameters from self.init_params
+        λ, μ, σ, γ, k, m = [self.init_params._asdict()[k] for k in ['λ', 'μ', 'σ', 'γ', 'k', 'm']]    
+        λ_old, μ_old, σ_old, γ_old, k_old, m_old = λ, μ, σ, γ, k, m
+    
+        ZFUN = lambda λ, μ, k, m: gamma_fun(m) * μ**m + gamma_fun(k) * λ**k
+        for i in range(max_iter):
+            Z = ZFUN(λ, μ, k, m)
+    
+            # E-step        
+            lrho       = 0 * y
+            lrho[y>=0] = -np.log(Z) + (k - 1)*np.log(y[y>=0]) - y[y>=0]/λ
+            lrho[y<0]  = -np.log(Z) + (m - 1)*np.log(-y[y<0]) - abs(y[y<0])/μ
+            leta       = -(y**2)/2/σ**2 - np.log(np.sqrt(2*np.pi*σ**2))
+            z          = (lrho - leta) > np.log((1-min(γ,1-1e-8))/γ)
+    
+            # M-step        
+            γ = (sum(z) + self.hyper_params.γ_pr_mean*self.hyper_params.γ_pr_strength*len(z))/(len(z) + self.hyper_params.γ_pr_strength*len(z))
+    
+            i0= ~z
+            n0= sum(i0)        
+            if n0>0:
+                y0 = y[i0]
+                if self.hyper_params.σ_penalty>0: σ2 = n0/(2*self.hyper_params.σ_penalty) * (-1 + np.sqrt(1 + 4 * self.hyper_params.σ_penalty * np.mean(y0**2)/n0))
+                else:                             σ2 = np.mean(y0**2)
+            else:
+                σ2 = np.var(y)*1e-3 # Don't make it exactly 0
+
+            σ = np.sqrt(σ2)
+                        
+            ip   = z & (y>=0)
+            i_n  = z & (y<0)    
+            n_p  = sum(ip)
+            nn   = sum(i_n)
+            n    = n_p + nn
+            yps  = sum(y[ip]) if n_p else 0
+            lyps = sum(np.log(y[ip])) if n_p else 0
+            yns  = sum(abs(y[i_n])) if nn else 0
+            lyns = sum(np.log(abs(y[i_n]))) if nn else 0
+
+            fixed_point_function = lambda x: np.array([
+                (ZFUN(x[0], x[2], x[1], x[3])/n/gamma_fun(x[1])/x[1]*yps)**(1/(x[1]+1)),
+                (np.log(x[0]) + digamma(x[1]))/x[0]*yps/lyps,
+                self.min_μ if nn==0 else (ZFUN(x[0], x[2], x[1], x[3])/n/gamma_fun(x[3])/x[3]*yns)**(1/(x[3]+1)),
+                1 if nn==0 else (np.log(x[2]) + digamma(x[3]))/x[2]*yns/lyns
+            ])
+
+            fixed_point_init = np.array([λ_old, k_old, μ_old, m_old])
+            
+            (λ, k, μ, m), status = fixed_point_iterate(fixed_point_function,
+                                                       fixed_point_init,
+                                                       damping  = damping,
+                                                       max_iter = max_fp_iter,)
+            
+            # Print the values at the current iteration, including the iteration number
+            self.params = self.Params(λ=λ, μ=μ, σ=σ, γ=γ, k=k, m=m)
+            INFO(f"Iter {i:>4d}: n+={n_p:>4d}, n-={nn:>4d} n0={len(ip) -nn - n_p:>4d} " + params2str(self.params))
+            # Check convergence
+            if abs(λ - λ_old) < tol and abs(μ - μ_old) < tol and abs(σ - σ_old) < tol and abs(γ - γ_old) < tol and abs(m - m_old) < tol and abs(k - k_old) < tol:
+                INFO("Converged.")
+                break
+            λ_old = λ
+            μ_old = μ
+            σ_old = σ
+            γ_old = γ
+            m_old = m
+            k_old = k
+    
+        labs = 0*z
+        labs[z & (y>=0)] = 1
+        labs[z & (y<0)]  = -1
+        self.labs = labs
+
+        return self
